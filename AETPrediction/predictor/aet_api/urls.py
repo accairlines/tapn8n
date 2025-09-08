9f01bc1c43b4e409a5536ff1e42333c49518e88a
PedroSarmento <PedroSarmento@PMRS>
2025-09-08 10:39:27 +0100

    New Version

--- AETPrediction/predictor/aet_api/urls.py
+++ AETPrediction/predictor/aet_api/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('predict/<int:flight_id>/', views.predict_flight, name='predict_flight'),
-    path('predict/batch/', views.predict_batch, name='predict_batch'),
-    path('extract-data/', views.extract_flight_data, name='extract_flight_data'),
+    path('predict/batch/', views.predict_batch, name='predict_batch')
 ] 
