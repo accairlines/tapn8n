8a60bf2b89ea1de4d4a7b01a76ec9b8455fb97a9
PedroSarmento <PedroSarmento@PMRS>
2025-09-09 10:31:48 +0100

    New Version

--- AETPrediction/trainer/preprocess.py
+++ AETPrediction/trainer/preprocess.py
@@ -3,10 +3,6 @@ Feature extraction and preprocessing functions
 """
 
 import pandas as pd
-import numpy as np
-from datetime import datetime
-
-import codification
 
 def preprocess_flight_data(flights):
     """Preprocess all flight features for model training: encode all columns as category codes, fill missing with -1."""
