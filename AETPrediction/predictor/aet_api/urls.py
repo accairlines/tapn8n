c0410d55fca7fd56e55033ba3093fcf621b66383
PedroSarmento <PedroSarmento@PMRS>
2026-01-08 16:12:35 +0000

    New Version

--- AETPrediction/predictor/aet_api/urls.py
+++ AETPrediction/predictor/aet_api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('predict/<int:flight_id>/', views.predict_flight, name='predict_flight'),
-    path('train_model/', views.train_model, name='train_model')
+    path('predict/<int:flight_id>/', views.predict_flight, name='predict_flight')
 ] 
